cb3185df7b3109ffe221810531b0ce455c464f8c
Santosh Kumar Rachakonda <san94rach@gmail.com>
2019-10-28 15:47:41 +0530

    Update app java
     Update app java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Boss Make Life! and Made it AmaZing" );
+        System.out.println( "Hello Boss Make Life! and Made it AmaZing CICD" );
     }
 }
